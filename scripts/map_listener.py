a30360b37f577a995d3990025d1a521365ec0a03
Tomasz Kucner <tomasz.kucner@oru.se>
2021-03-09 15:55:24 +0100

    added minimum working example with some paramitters in alunch file

--- scripts/map_listener.py
+++ scripts/map_listener.py
@@ -6,38 +6,50 @@ import numpy as np
 from rose.fft_structure_extraction import FFTStructureExtraction as structure_extraction
 
 
-pub = rospy.Publisher('filtered_map', OccupancyGrid, queue_size=10, latch=True)
-pub_map = OccupancyGrid()
+class rose_wrapper:
+    def __init__(self,params):
+        self.pub_map = OccupancyGrid()
+        self.params=params
 
-def occupancygrid_to_numpy(msg):
-    data = np.asarray(msg.data, dtype=np.int8).reshape(msg.info.height, msg.info.width)
-    data = np.where(data==-1, 128, data)
-    data = np.where(data==0, 255, data)
-    data = np.where(data==100, 0, data)
-    rose = structure_extraction(data, peak_height=0.05, smooth=True ,sigma=0.5)
-    rose.process_map()
-    rose.simple_filter_map(0.35)
-    result = rose.analysed_map*1
-    result = np.where(result==1, 100, result)
-    result=result[:msg.info.height, :msg.info.width]
-    print("Procesed")
-    print(result)
-    return result
+    def filter_map(self,msg):
+        data = np.asarray(msg.data, dtype=np.int8).reshape(msg.info.height, msg.info.width)
+        data = np.where(data==-1, 128, data)
+        data = np.where(data==0, 255, data)
+        data = np.where(data==100, 0, data)
 
-def callback(msg):
-    map_filtered = occupancygrid_to_numpy(msg)
-    pub_map.header = msg.header
-    pub_map.info = msg.info
-    pub_map.data=list(map_filtered.ravel())
+        rose = structure_extraction(data, peak_height=self.params['peak_height'], smooth=self.params['smooth'] , sigma=self.params['sigma'])
+        rose.process_map()
+        if self.params['filtering_tr']>0:
+            rose.simple_filter_map(self.params['filtering_tr'])
+        else:
+            rose.histogram_filtering()
+        result = rose.analysed_map*1
+        result = np.where(result==1, 100, result)
+        result = result[:msg.info.height, :msg.info.width]
+        self.pub_map.header = msg.header
+        self.pub_map.info = msg.info
+        self.pub_map.data=list(result.ravel())
+
 
 def listener():
 
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber('map', OccupancyGrid, callback)
+    params=dict()
+    params['sub_map_topic_name']=rospy.get_param("~input_map", "map")
+    params['pub_map_topic_name']=rospy.get_param("~output_map", "o_map")
+    params['filtering_tr']=rospy.get_param("~threshold", -1.0)
+    params['peak_height']=rospy.get_param("~peak_heigh", 0.05)
+    params['smooth']=rospy.get_param("~smooth", True)
+    params['sigma']=rospy.get_param("~sigma", 0.5)
+
+    rw = rose_wrapper(params)
+
+    pub= rospy.Publisher(params['pub_map_topic_name'], OccupancyGrid, queue_size=10, latch=True)
+
+    rospy.Subscriber(params['sub_map_topic_name'], OccupancyGrid, rw.filter_map)
+
     while not rospy.is_shutdown():
-        pub.publish(pub_map)
-
-
+        pub.publish(rw.pub_map)
 
     rospy.spin()
 
